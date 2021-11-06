6fb0f8c141f400ab584f22c1e9bf6460475356ae
nedimazar <nedimazar@gmail.com>
2021-11-06 03:18:00 +0100

    Gonna need to refactor

--- src/SAT.py
+++ src/SAT.py
@@ -2,50 +2,36 @@ import numpy as np
 
 class SATSolver:
 
-    def __init__(self, rules, puzzle, board_size = 9, multi=False):
+    def __init__(self, rules, puzzle, board_size = 9):
         self.filename = puzzle
         #self.rules = self.read_dimacs(rules)
         self.board_size = board_size
-        if not multi:
-            self.puzzle = self.read_sudoku(puzzle)
-        else:
-            self.puzzles = self.read_multi_sudoku(puzzle)
+        self.puzzles = self.read_sudoku(puzzle)
     
-    def read_multi_sudoku(self, filename):
-        with open(filename, 'r') as input_file:
-            data = input_file.read()
-            puzzles = self.get_multi_puzzle(data)
-        return puzzles
-
-    def get_multi_puzzle(self, data):
-        boards = []
-        while data:
-            board = np.zeros(shape=(self.board_size, self.board_size))
-            for i in range(self.board_size):
-                for j in range(self.board_size):
-                    board[i][j] = data[0] if data and str.isnumeric(data[0]) else 0
-                    data = data[1:]
-            boards.append(board)
-        return boards
+    def write_dimacs(self, puzzle):
+        if True:
+            name = self.filename.split("/")[1]
+            with open(f'out/{name}.out', 'w') as out:
+                for i, row in enumerate(puzzle):
+                    for j, pos in enumerate(row):
+                        if pos > 0:
+                            out.write(f"{i+1}{j+1}{int(pos)} 0\n")
 
     def read_sudoku(self, filename):
+        boards = []
         with open(filename, 'r') as input_file:
-            data = input_file.read()
-            puzzle = self.get_puzzle(data)
-        return puzzle
-    
-    def get_puzzle(self, data):
-        board = np.zeros(shape=(self.board_size, self.board_size))
-        for i in range(self.board_size):
-            for j in range(self.board_size):
-                board[i][j] = data[0] if str.isnumeric(data[0]) else 0
-                data = data[1:]
-        return board
+            lines = input_file.readlines()
+            i = 0
+            for line in lines:
+                stripped = line.strip("\n")
+                board = np.zeros(shape=(self.board_size, self.board_size))
+                x = 0
+                for i in range(self.board_size):
+                    for j in range(self.board_size):
+                        board[i][j] = stripped[x] if str.isnumeric(stripped[x]) else 0
+                        x+=1
+                boards.append(board)
+        return boards
 
-                
-        
-
-x = SATSolver(None, 'sudoku/1000_sudokus.txt', board_size=9, multi=True)
-
-for board in x.puzzles:
-    print(board)
+x = SATSolver(None, 'sudoku/1000_sudokus.txt', board_size=9)
+x.write_dimacs(x.puzzles[75])
